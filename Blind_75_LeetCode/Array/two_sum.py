b9c017ef6b3cef52d605d3fbb5b27f5cafd4d5e5
nagaratnaraikar <66009515+nagaratnaraikar@users.noreply.github.com>
2024-12-29 22:33:41 +0530

    Update two_sum.py

--- Blind_75_LeetCode/Array/two_sum.py
+++ Blind_75_LeetCode/Array/two_sum.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def twosum(self, nums, target):
+        n = len(nums)
+        for i in range(n-1):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+result = Solution()
+nums = [2,7,11,15]
+target = 9
+result1 = result.twosum(nums, target)
+print(result1)
